43ec06ae6675639f3e224b9c18f9f98ef1993752
Disrrptt <gustavovilleth@gmail.com>
2023-09-09 03:31:54 -0300

    Adicionei a função obterNota para obter notas válidas no intervalo de 0 a 10 e adicionei um loop.

--- main.c
+++ main.c
@@ -19,6 +19,41 @@ const char *determinarSituacao(float media) {
     return (media >= 7.0) ? "APROVADO" : "REPROVADO";
 }
 
+int obterTelefoneNumerico(char *telefone, int tamanho) {
+    printf("Digite o telefone do aluno (apenas números): ");
+    fgets(telefone, tamanho, stdin);
+    telefone[strcspn(telefone, "\n")] = '\0'; // Remove a quebra de linha
+
+    // Validação do telefone - garantindo que todos os caracteres sejam dígitos
+    int len = strlen(telefone);
+    for (int i = 0; i < len; i++) {
+        if (!isdigit(telefone[i])) {
+            return 0; // Retorna 0 para indicar erro
+        }
+    }
+    return 1; // Retorna 1 para indicar sucesso
+}
+
+int obterNota(float *nota, const char *nomeDaNota) {
+    char notaStr[10]; // Usado para armazenar temporariamente a entrada das notas
+
+    while (1) {
+        printf("Digite a %s do aluno: ", nomeDaNota);
+        fgets(notaStr, sizeof(notaStr), stdin);
+        notaStr[strcspn(notaStr, "\n")] = '\0'; // Remove a quebra de linha
+
+        if (sscanf(notaStr, "%f", nota) == 1) {
+            if (*nota >= 0 && *nota <= 10) {
+                return 1; // Retorna 1 para indicar sucesso
+            } else {
+                printf("Nota inválida. Deve estar no intervalo de 0 a 10.\n");
+            }
+        } else {
+            printf("Nota inválida. Use apenas números.\n");
+        }
+    }
+}
+
 int main() {
     FILE *saida = fopen("SituacaoFinal.csv", "w");
     if (saida == NULL) {
@@ -33,41 +68,16 @@ int main() {
     fgets(aluno.nome, sizeof(aluno.nome), stdin);
     aluno.nome[strcspn(aluno.nome, "\n")] = '\0'; // Remove a quebra de linha
 
-    printf("Digite o telefone do aluno (apenas números): ");
-    fgets(aluno.telefone, sizeof(aluno.telefone), stdin);
-    aluno.telefone[strcspn(aluno.telefone, "\n")] = '\0'; // Remove a quebra de linha
-
-    // Validação do telefone - garantindo que todos os caracteres sejam dígitos
-    int len = strlen(aluno.telefone);
-    for (int i = 0; i < len; i++) {
-        if (!isdigit(aluno.telefone[i])) {
-            printf("Telefone inválido. Use apenas números.\n");
-            fclose(saida);
-            return 1;
-        }
+    while (!obterTelefoneNumerico(aluno.telefone, sizeof(aluno.telefone))) {
+        printf("Telefone inválido. Use apenas números.\n");
     }
 
     printf("Digite o curso do aluno: ");
     fgets(aluno.curso, sizeof(aluno.curso), stdin);
     aluno.curso[strcspn(aluno.curso, "\n")] = '\0'; // Remove a quebra de linha
 
-    char notaStr[10]; // Usado para armazenar temporariamente a entrada das notas
-
-    printf("Digite a nota 1 do aluno: ");
-    fgets(notaStr, sizeof(notaStr), stdin);
-    if (sscanf(notaStr, "%f", &aluno.nota1) != 1) {
-        printf("Nota 1 inválida. Use apenas números.\n");
-        fclose(saida);
-        return 1;
-    }
-
-    printf("Digite a nota 2 do aluno: ");
-    fgets(notaStr, sizeof(notaStr), stdin);
-    if (sscanf(notaStr, "%f", &aluno.nota2) != 1) {
-        printf("Nota 2 inválida. Use apenas números.\n");
-        fclose(saida);
-        return 1;
-    }
+    while (!obterNota(&aluno.nota1, "nota 1"));
+    while (!obterNota(&aluno.nota2, "nota 2"));
 
     float media = calcularMedia(aluno.nota1, aluno.nota2);
     const char *situacao = determinarSituacao(media);
