844758c390a5f2063ed6dd0813b7114b150fff9b
Disrrptt <gustavovilleth@gmail.com>
2023-09-09 03:26:34 -0300

    versão aprimorada do código adiciona mais validação e tratamento de erros

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-// Definição da estrutura Aluno
 typedef struct {
     char nome[100];
     char telefone[15];
@@ -11,61 +11,69 @@ typedef struct {
     float nota2;
 } Aluno;
 
-// Função para calcular a média de duas notas
 float calcularMedia(float nota1, float nota2) {
     return (nota1 + nota2) / 2.0;
 }
 
-// Função para determinar a situação do aluno
 const char *determinarSituacao(float media) {
     return (media >= 7.0) ? "APROVADO" : "REPROVADO";
 }
 
 int main() {
-    // Abertura do arquivo de saída para escrita
     FILE *saida = fopen("SituacaoFinal.csv", "w");
     if (saida == NULL) {
         perror("Erro ao abrir o arquivo de saída");
         return 1;
     }
 
-    // Escreve o cabeçalho no arquivo de saída
     fprintf(saida, "Nome,Telefone,Curso,Nota Média,Situação\n");
 
-    // Solicita os dados do aluno ao usuário
     Aluno aluno;
     printf("Digite o nome do aluno: ");
     fgets(aluno.nome, sizeof(aluno.nome), stdin);
+    aluno.nome[strcspn(aluno.nome, "\n")] = '\0'; // Remove a quebra de linha
 
-    // Solicita o telefone do aluno, aceitando apenas números
     printf("Digite o telefone do aluno (apenas números): ");
     fgets(aluno.telefone, sizeof(aluno.telefone), stdin);
+    aluno.telefone[strcspn(aluno.telefone, "\n")] = '\0'; // Remove a quebra de linha
 
-    // Remove os caracteres não numéricos do telefone
+    // Validação do telefone - garantindo que todos os caracteres sejam dígitos
     int len = strlen(aluno.telefone);
     for (int i = 0; i < len; i++) {
-        if (aluno.telefone[i] < '0' || aluno.telefone[i] > '9') {
-            aluno.telefone[i] = '\0'; // Substitui não números por terminador nulo
+        if (!isdigit(aluno.telefone[i])) {
+            printf("Telefone inválido. Use apenas números.\n");
+            fclose(saida);
+            return 1;
         }
     }
 
     printf("Digite o curso do aluno: ");
     fgets(aluno.curso, sizeof(aluno.curso), stdin);
+    aluno.curso[strcspn(aluno.curso, "\n")] = '\0'; // Remove a quebra de linha
+
+    char notaStr[10]; // Usado para armazenar temporariamente a entrada das notas
 
     printf("Digite a nota 1 do aluno: ");
-    scanf("%f", &aluno.nota1);
+    fgets(notaStr, sizeof(notaStr), stdin);
+    if (sscanf(notaStr, "%f", &aluno.nota1) != 1) {
+        printf("Nota 1 inválida. Use apenas números.\n");
+        fclose(saida);
+        return 1;
+    }
 
     printf("Digite a nota 2 do aluno: ");
-    scanf("%f", &aluno.nota2);
+    fgets(notaStr, sizeof(notaStr), stdin);
+    if (sscanf(notaStr, "%f", &aluno.nota2) != 1) {
+        printf("Nota 2 inválida. Use apenas números.\n");
+        fclose(saida);
+        return 1;
+    }
 
-    // Calcula a média das notas do aluno e determina a situação
     float media = calcularMedia(aluno.nota1, aluno.nota2);
     const char *situacao = determinarSituacao(media);
 
-    // Escreve os dados no arquivo de saída
     fprintf(saida, "%s,%s,%s,%.2f,%s\n", aluno.nome, aluno.telefone, aluno.curso, media, situacao);
 
-    // Fecha o arquivo de saída
     fclose(saida);
 
     printf("Processamento concluído. Resultados salvos em SituacaoFinal.csv\n");
